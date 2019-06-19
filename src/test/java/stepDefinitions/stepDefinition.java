29431bd93e9bba57c28c51ea588960b0a6becb5d
jaysondizon <jayson.dizon15@gmail.com>
2019-06-19 14:16:04 +1000

    test

--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -7,11 +7,10 @@ import cucumber.api.junit.Cucumber;
 import files.payLoad;
 import files.resources;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
+
+
+import static io.restassured.RestAssured.given;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -45,7 +44,9 @@ public class stepDefinition {
 	
 	@When("^User view his pet inventory$")
 	public void user_view_his_pet_inventory() throws Throwable {
-		i_have_succesfully_logged_in_my_account();
+		given().
+		queryParam("api_key",prop.getProperty("KEY")).
+		when().
 		given().get(resources.inventory());
 		
 	    
@@ -53,7 +54,6 @@ public class stepDefinition {
 	
 	@Then("^Inventory of pets by status successfully shown$")
 	public void inventory_of_pets_by_status_successfully_shown() throws Throwable {
-		user_view_his_pet_inventory();
 		given().then().statusCode(200);
 		
 	}
@@ -69,14 +69,12 @@ public class stepDefinition {
 
 	@When("^The order has been placed$")
 	public void the_order_has_been_placed() throws Throwable {
-		the_details_of_the_order();
 		given().post(resources.placeorder());
 
 	}
 
 	@Then("^Order succesfully created with the provided details$")
 	public void order_succesfully_created_with_the_provided_details() throws Throwable {
-		the_order_has_been_placed();
 		given().then().statusCode(200);
 	}
 
@@ -90,13 +88,11 @@ public class stepDefinition {
 
 	@When("^Submit the order details$")
 	public void submit_the_order_details() throws Throwable {
-		the_invalid_details_of_the_order();
 		given().post(resources.placeorder());
 	}
 
 	@Then("^Order is not created$")
 	public void order_is_not_created() throws Throwable {
-		submit_the_order_details();
 		given().then().statusCode(400);
 	}
 
@@ -110,14 +106,12 @@ public class stepDefinition {
 
 	@When("^User search for the order$")
 	public void user_search_for_the_order() throws Throwable {
-		the_reference_number_of_an_existing_order();
 		given().get(resources.queryorder());
 	}
 
 	@Then("^Details of the order shown successfully$")
 	public void details_of_the_order_shown_successfully() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		user_search_for_the_order();
 		given().then().statusCode(200);
 	}
 
@@ -131,14 +125,12 @@ public class stepDefinition {
 
 	@When("^User deletes the order$")
 	public void user_deletes_the_order() throws Throwable {
-		the_reference_number_of_existing_order();
 		given().delete(resources.deleteorder());
 	}
 
 	@Then("^Order successfully deleted$")
 	public void order_successfully_deleted() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		user_deletes_the_order();
 		given().then().statusCode(400);
 	}
 
